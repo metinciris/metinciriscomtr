522eb1e3272cfecf0450f02942c946265cce8e02
Metin ÇİRİŞ <ibrahimciris@sdu.edu.tr>
2025-11-22 13:07:28 +0300

    Update base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: If deploying to https://<USERNAME>.github.io/<REPO>/, set base to '/<REPO>/'
-  // base: '/yenisitem/', 
+  base: '/metinciriscomtr/',
 })
