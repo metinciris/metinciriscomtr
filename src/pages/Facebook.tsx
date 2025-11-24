49e5087898fb320a2cfc63b882e041c331eb5fb5
Metin ÇİRİŞ <ibrahimciris@sdu.edu.tr>
2025-11-24 21:07:24 +0300

    Update Facebook.tsx

--- src/pages/Facebook.tsx
+++ src/pages/Facebook.tsx
@@ -35,7 +35,7 @@ export function Facebook() {
     return (
         <PageContainer>
             {/* Header */}
-            <div className="bg-gradient-to-r from-[#3B5998] to-[#8b9dc3] text-white p-12 mb-8 rounded-xl shadow-lg">
+            <div className="bg-[#3B5998] text-white p-12 mb-8 rounded-xl shadow-lg">
                 <div className="flex items-center gap-4 mb-4">
                     <svg className="w-12 h-12" fill="currentColor" viewBox="0 0 24 24">
                         <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
