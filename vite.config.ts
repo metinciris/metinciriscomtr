5c4d0fac2cff1eb0f0cb33f2a9066985b1146de1
Metin ÇİRİŞ <ibrahimciris@sdu.edu.tr>
2025-11-25 00:59:27 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/',
+    base: '/metinciriscomtr/', 
   resolve: {
     alias: {
       'vaul@1.1.2': 'vaul',
